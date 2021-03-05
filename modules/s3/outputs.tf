1b0d8fe60b11a754c5456a9f6a53610553f7db46
max <maxmiller.silva@b2wdigital.com>
2021-03-05 05:48:01 -0300

    chore: add comment to s3 output file

--- modules/s3/outputs.tf
+++ modules/s3/outputs.tf
@@ -1,3 +1,8 @@
+/*
+  Quando criamos um recurso podemos disponibilizar os dados dele como variavel, neste caso, atribuímos bucket_arn o valor do arn
+  do nosso S3, isso vai ser utilizado posteriormente pelo kinesis para saber para qual bucket enviar os dados
+  https://www.terraform.io/docs/language/values/outputs.html
+*/
 output "bucket_arn" {
   value       = aws_s3_bucket.raw_bucket.arn
   description = "S3 destination Bucket ARN"
